b570a837279b6282029c4b1f1f041a084eb3ff46
sugarl-sudo <sato.yuta0112@gmail.com>
2024-09-03 17:23:41 +0900

    結果の保存しない

--- train.sh
+++ train.sh
@@ -1,6 +1,8 @@
 # AT
-CUDA_VISIBLE_DEVICES=0 python train_cifar.py --model "deit_tiny_patch16_224" --out-dir "./pgd_vanilla" --method 'AT' --seed 0
+# CUDA_VISIBLE_DEVICES=0 python train_cifar.py --model "deit_tiny_patch16_224" --out-dir "./pgd_vanilla" --method 'AT' --seed 0
 # TRADES
-# CUDA_VISIBLE_DEVICES=1 python train_cifar.py --model "deit_tiny_patch16_224" --out-dir "./trades_vanilla" --method 'TRADES' --seed 0
-# # MART
-# CUDA_VISIBLE_DEVICES=2 python train_cifar.py --model "deit_tiny_patch16_224" --out-dir "./mart_vanilla" --method 'MART' --seed 0
+# CUDA_VISIBLE_DEVICES=1 python train_cifar.py --model "deit_tiny_patch16_224" --out-dir "./trades_vanilla" --method 'TRADES' --seed 0 
+# MART
+CUDA_VISIBLE_DEVICES=2 python train_cifar.py --model "deit_tiny_patch16_224" --out-dir "./results/mart_vanilla" --method 'MART' --seed 0 > mart_vanilla.log 2>&1 & 
+# TRADES + deit_small 
+CUDA_VISIBLE_DEVICES=3 python train_cifar.py --model "deit_small_patch16_224" --out-dir "./results/trades_vanilla" --method 'TRADES' --seed 0 > trades_vanilla.log 2>&1 & 
