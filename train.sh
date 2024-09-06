96c10c1335ddd4276856b60e7527e911ec78b060
sugarl-sudo <sato.yuta0112@gmail.com>
2024-09-06 15:12:02 +0900

    無駄な実行の削除

--- train.sh
+++ train.sh
@@ -3,6 +3,6 @@
 # TRADES
 # CUDA_VISIBLE_DEVICES=1 python train_cifar.py --model "deit_tiny_patch16_224" --out-dir "./trades_vanilla" --method 'TRADES' --seed 0 
 # MART
-CUDA_VISIBLE_DEVICES=2 python train_cifar.py --model "deit_tiny_patch16_224" --out-dir "./results/mart_vanilla" --method 'MART' --seed 0 > mart_vanilla.log 2>&1 & 
+# CUDA_VISIBLE_DEVICES=2 python train_cifar.py --model "deit_tiny_patch16_224" --out-dir "./results/mart_vanilla" --method 'MART' --seed 0 > mart_vanilla.log 2>&1 & 
 # TRADES + deit_small 
-CUDA_VISIBLE_DEVICES=3 python train_cifar.py --model "deit_small_patch16_224" --out-dir "./results/trades_vanilla" --method 'TRADES' --seed 0 > trades_vanilla.log 2>&1 & 
+# CUDA_VISIBLE_DEVICES=3 python train_cifar.py --model "deit_small_patch16_224" --out-dir "./results/trades_vanilla" --method 'TRADES' --seed 0 > trades_vanilla.log 2>&1 & 
